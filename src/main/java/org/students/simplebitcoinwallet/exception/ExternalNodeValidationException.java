a8ac777de2c96f92a3676821eb8e9b99356a268d
toomistm <martin.toomiste0@gmail.com>
2024-05-17 15:33:51 +0300

    Create BitcoinNodeAPIService implementation

--- src/main/java/org/students/simplebitcoinwallet/exception/ExternalNodeValidationException.java
+++ src/main/java/org/students/simplebitcoinwallet/exception/ExternalNodeValidationException.java
@@ -1,10 +1,16 @@
 package org.students.simplebitcoinwallet.exception;
 
+import lombok.Getter;
+import org.students.simplebitcoinwallet.data.ValidationErrorResponse;
+
 /**
  * Thrown when external node responds with validation errors
  */
+@Getter
 public class ExternalNodeValidationException extends ExternalNodeResponseException{
-    public ExternalNodeValidationException(String message) {
+    private final ValidationErrorResponse validationErrorResponse;
+    public ExternalNodeValidationException(String message, ValidationErrorResponse validationErrorResponse) {
         super(message);
+        this.validationErrorResponse = validationErrorResponse;
     }
 }
